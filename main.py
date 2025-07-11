620c87f09b0975e0fde1b076294ab4709ba21aee
Weston Platter <weston@westonplatter.com>
2025-07-11 14:40:41 -0600

    Fixed! Updated the JP Morgan CIK to the correct format `"0001485894"` in main.py:9.

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from fh.sec_client import SECHTTPClient
 CIK_MAP = {
     # fund issuer => cik
     "ishares": "1100663",
-    "jpmorgan": "1485894",
+    "jpmorgan": "0001485894",
 }
 
 def download_series_data(client, cik):
